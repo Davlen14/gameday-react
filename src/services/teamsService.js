0176636caa313472f792d8d1bbc0161082ed3280
Davlen14 <99432647+Davlen14@users.noreply.github.com>
2025-02-17 16:38:08 -0600

    Refactored Latest Updates layout, resolved CSS conflicts, improved responsiveness

--- src/services/teamsService.js
+++ src/services/teamsService.js
@@ -275,7 +275,11 @@ export const fetchScoreboard = async (year, week) => {
     return await fetchData(endpoint, params);
 };
 
-
+export const getAllRecruits = async (year = 2025) => {
+    const endpoint = "/recruiting/players";
+    const params = { year };
+    return await fetchData(endpoint, params);
+};
 
 // Export all functions
 const teamsService = {
