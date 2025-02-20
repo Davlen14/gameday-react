f813abe856c797cda6b85ae5f7e6041d40339402
Davlen14 <99432647+Davlen14@users.noreply.github.com>
2025-02-20 17:17:46 -0600

    feat: add YouTube video highlights for college football and Joel Klatt

--- src/components/TeamAnalyticsDetail.js
+++ src/components/TeamAnalyticsDetail.js
@@ -97,59 +97,61 @@ const TeamAnalyticsDetail = () => {
   return (
     <div className="min-h-screen w-full bg-gray-100 flex flex-col">
       {/* --- Scoreboard Section --- */}
-      <div className="relative flex items-center justify-between bg-white py-4 shadow">
+      <div className="relative flex items-center justify-between bg-white h-20 shadow">
         {/* Left diagonal color (Away color) */}
         <div
-          className="absolute left-0 top-0 h-full w-12"
+          className="absolute left-0 top-0 h-full w-8"
           style={{
             backgroundColor: awayColor,
-            clipPath: "polygon(0 0, 100% 0, 85% 100%, 0 100%)",
+            clipPath: "polygon(0 0, 100% 0, 80% 100%, 0 100%)",
           }}
         ></div>
 
         {/* Right diagonal color (Home color) */}
         <div
-          className="absolute right-0 top-0 h-full w-12"
+          className="absolute right-0 top-0 h-full w-8"
           style={{
             backgroundColor: homeColor,
-            clipPath: "polygon(15% 0, 100% 0, 100% 100%, 0 100%)",
+            clipPath: "polygon(20% 0, 100% 0, 100% 100%, 0 100%)",
           }}
         ></div>
 
         {/* Away Team */}
-        <div className="flex items-center pl-16 space-x-3">
+        <div className="flex items-center pl-16 space-x-2">
           <img
             src={awayLogo}
             alt={game.awayTeam}
-            className="w-10 h-10 object-contain"
+            className="w-8 h-8 object-contain"
           />
-          <div className="text-left">
-            <div className="font-bold text-base">{game.awayTeam}</div>
+          <div className="text-left text-sm">
+            <div className="font-semibold">{game.awayTeam}</div>
+            {/* Points (if available) */}
             {game.awayPoints !== undefined && (
-              <div className="text-sm">{game.awayPoints}</div>
+              <div className="text-xs">{game.awayPoints}</div>
             )}
           </div>
         </div>
 
         {/* Game Info (center) */}
-        <div className="text-center">
-          <div className="font-bold">{gameDate}</div>
-          <div className="text-sm">{gameTime}</div>
+        <div className="text-center text-sm">
+          <div className="font-semibold">{gameDate}</div>
+          <div>{gameTime}</div>
           <div className="text-xs mt-1">{game.venue}</div>
         </div>
 
         {/* Home Team */}
-        <div className="flex items-center pr-16 space-x-3">
-          <div className="text-right">
-            <div className="font-bold text-base">{game.homeTeam}</div>
+        <div className="flex items-center pr-16 space-x-2">
+          <div className="text-right text-sm">
+            <div className="font-semibold">{game.homeTeam}</div>
+            {/* Points (if available) */}
             {game.homePoints !== undefined && (
-              <div className="text-sm">{game.homePoints}</div>
+              <div className="text-xs">{game.homePoints}</div>
             )}
           </div>
           <img
             src={homeLogo}
             alt={game.homeTeam}
-            className="w-10 h-10 object-contain"
+            className="w-8 h-8 object-contain"
           />
         </div>
       </div>
@@ -166,6 +168,7 @@ const TeamAnalyticsDetail = () => {
 };
 
 export default TeamAnalyticsDetail;
+
 
 
 
